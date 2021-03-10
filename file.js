66403b7f0605895c209ecf65ee08ee0afe20ce29
rahajawadi <raha.jawadi@gmail.com>
2021-03-10 20:08:41 +0430

    Adding the first File!

---
+++ file.js
@@ -0,0 +1,65 @@
+//alert ("Hello World");
+/*  document.write("Hello World");
+
+  var age = 10;
+  if( age < 10) {
+    document.write(" You are elibible for this peogram! ");
+  }
+  else if (age >10 ){
+    document.write("Sorry, You are not eligible!");
+  }
+  else{
+    document.write("Maybe you can join the program!");
+  }
+
+  var myDate = new Date();
+  console.log(myDate);
+  console.log(2+5);
+
+  var a = "ABC";
+  var b = "abc";
+  document.write(b);
+  document.write(b.toUpperCase());
+
+  var result = a.indexOf("M");
+  if (result === -1){
+    document.write("Not Existed");
+  } else {
+    document.write(result);
+  }
+
+// Changing from lowerCase to upperCase
+  console.log(a.toLowerCase() === b.toLowerCase());
+
+// slicing a certain string into different parts
+var first = "Hello My Dear";
+var sec= first.slice(3,6);
+document.write(sec);
+
+// spiliting values of an string
+ var all = "name, familyName, lastName";
+  document.write(all);
+  console.log(all);
+ var spilitAll = all.split(",");
+ document.write(spilitAll);
+ console.log(spilitAll);
+ // Printing the array in reverse order
+  console.log(spilitAll.reverse());
+
+//
+var side1= 5;
+var side2= 6;
+var side3= 7;
+
+var s = (side1+side2+side3)/2;
+var area = Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+document.write(area);
+*/
+
+var a = prompt("Please enter your first number");
+var b = prompt("Please enter the second number");
+a=parseInt(a);
+b=parseInt(b);
+z= a+b;
+document.write(z);
+alert(z);
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+$name = "The world is very small ";
+
+echo "Hello world!";
+echo "<Br>";
+echo $name."Go to the hell";
+?>
