b3194144c9cf560d2b8a04634ce01e9b85d3262c
morsal jan <aho.sahra2020@gmail.com>
2021-03-11 08:39:11 -0800

    a fuction added

--- file.js
+++ file.js
@@ -63,3 +63,11 @@ b=parseInt(b);
 z= a+b;
 document.write(z);
 alert(z);
+
+
+// new comment 
+
+
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
